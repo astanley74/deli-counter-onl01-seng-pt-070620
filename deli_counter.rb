2e199a2dc66e0d203c34458bc6ef9e4f13650785
Austin Stanley <stanley.377@buckeyemail.osu.edu>
2020-07-08 23:45:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,5 +4,6 @@ def line(katz_deli)
   if katz_deli.empty?
     "The line is currently empty."
   else
-    new_line = "The line is currently: "
+    new_line = "The line is currently:"
+    katz_deli.
     
